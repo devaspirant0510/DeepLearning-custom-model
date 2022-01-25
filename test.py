68f3c0dd136155ba8f0018356f1a2cf3e659d868
Seung <68223593+devaspirant0510@users.noreply.github.com>
2022-01-25 22:14:08 +0900

    함수형으로 변경

--- test.py
+++ test.py
@@ -12,45 +12,19 @@ from imblearn.over_sampling import SMOTE
 from collections import Counter
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
+import config
 
-file_path = "파일경로"
-df = pd.read_excel(file_path)  # 파일 읽기
-df = df[:1000]
-
-x_train = df.iloc[:, :-2]  # y 값 제외하고 슬라이싱 , 고장단계도 제외(1,2,3 은 고장 유무가 0이고 4,5는 고장 유무가 1이기때문에 제거)
-y_train = df.iloc[:, -1]  # y 값 만 슬라이싱
-
-scaler = StandardScaler()
-scaler.fit(x_train)
-x_train = scaler.transform(x_train)
-
-ros = SMOTE(random_state=42)
-x_train = pd.DataFrame(x_train, columns=df.columns[:-2])
-X_res, y_res = ros.fit_resample(x_train, y_train)
-print('Resampled dataset shape %s' % Counter(y_res))
-
-train_x, test_x, train_y, test_y = train_test_split(X_res, y_res, random_state=42, stratify=y_res, test_size=0.3)
-"""train 에서 한번더 나누어줌 train,validation"""
-train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, random_state=42, stratify=train_y, test_size=0.3)
-
-# sel = SelectFromModel(RandomForestClassifier(n_estimators=1230))
-# sel.fit(x_train, y_train)
-selected_feat = x_train.columns[()]
-print(len(selected_feat))  # 18
-print(selected_feat)
-
-# 원본데이터에서 추출된 Feature 만 슬라이싱
-train_x = train_x.loc[:, selected_feat]
-test_x = test_x.loc[:, selected_feat]
+model_name = input("모델 이름을 입력하세요")
 
 
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear1 = nn.Linear(len(selected_feat), 200)  # len(selected_feat 바꿔줌
+        self.linear1 = nn.Linear(len(selected_feat), 200)
         self.linear2 = nn.Linear(200, 50)
         self.linear3 = nn.Linear(50, 1)
-        self.relu = nn.LeakyReLU()
+        self.relu = nn.ReLU()
+        self.mish = nn.Mish()
         self.sigmoid = nn.Sigmoid()
         self.dropout = nn.Dropout(0.5)
 
@@ -70,134 +44,159 @@ class Model(nn.Module):
         return x
 
 
-train_x = torch.FloatTensor(train_x.to_numpy())
-train_y = torch.FloatTensor(train_y.to_numpy())
-test_x = torch.FloatTensor(test_x.to_numpy())
-test_y = torch.FloatTensor(test_y.to_numpy())
-"""validation 값도 FloatTensor 로 변환"""
-val_x = torch.FloatTensor(val_x.to_numpy())
-val_y = torch.FloatTensor(val_y.to_numpy())
+def read_file(path: str) -> pd.DataFrame:
+    df = pd.read_excel(path)  # 파일 읽기
+    df = df[:1000]
+    return df
 
-train_dataset = TensorDataset(train_x, train_y)
 
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+def my_feature_selection(x_data: pd.DataFrame, y_data: pd.DataFrame) -> [pd.DataFrame, pd.Index]:
+    sel = SelectFromModel(RandomForestClassifier(n_estimators=1230))
+    sel.fit(x_data, y_data)
+    selected_feat = x_data.columns[(sel.get_support())]
+    print(len(selected_feat))
+    print(selected_feat)
+    # 원본데이터에서 추출된 Feature 만 슬라이싱
+    return x_data.loc[:, selected_feat], selected_feat
 
-test_dataset = TensorDataset(test_x, test_y)
 
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
-"""변환시킨 Tensor 를 DataLoader 로 만들어줌"""
-val_dataset = TensorDataset(val_x, val_y)
+def my_over_sampling(x_data: pd.DataFrame, y_data: pd.DataFrame) -> [pd.DataFrame, pd.Series]:
+    smote = SMOTE(random_state=42)
+    x_data_smote, y_data_smote = smote.fit_resample(x_data, y_data)
+    return [x_data_smote, y_data_smote]
 
-val_loader = DataLoader(val_dataset, batch_size=32, shuffle=True)
 
-epoch = 2000
-lr = 0.01
-loss_list = []  # loss 값을 저장할 리스트
-valid_list = []
-acc_list = []  # acc 값을 저장할 리스트
-f1_list = []  # f1 score 값을 저장할 리스트
+def my_scaler(x_train: pd.DataFrame, x_test: pd.DataFrame) -> [np.array, np.array]:
+    sc = StandardScaler()
+    fit_x_train = sc.fit_transform(x_train)
+    fit_x_test = sc.transform(x_test)
+    return fit_x_train, fit_x_test
 
-model = Model()
-cost_func = nn.BCELoss()  # 이진분류기 때문에 binary cross entropy 사용
-optimizer = optim.SGD(model.parameters(), lr=lr)  # 경사하강법은 SGD 알고리즘 사용
 
-for ep in range(1, epoch + 1):
-    acc_data = 0  # 1 epoch accuracy
-    loss_data = 0  # 1 epoch loss
-    f1_data = 0  # 1 epoch f1 score
-    valid_loss =0
-    total = 0
-    f1_total = 0
-    """학습모드"""
-    model.train()
-    for x, y in train_loader:
-        pred = model(x)
-        loss = cost_func(pred, y.reshape(-1, 1))
-        optimizer.zero_grad()
-        loss.backward()  # backpropagation
-        optimizer.step()  # weight bias update
-        # 예측값이 0.5 이상은 1로 처리 0.5 이하는 0 로 처리
-        # 실제 정답값과 비교하여 accuracy 구함
-        y_pred = pred.detach().numpy()
-        y_pred = np.where(y_pred >= 0.5, 1, 0)
-        y_true = y.detach().numpy().reshape(-1, 1)
-        acc_data += accuracy_score(y_true, y_pred)
-        # accuracy 를 구하기 위해 전체 데이터 사이즈 더함
-        total += y.size(0)
-        # loss 값 더함
-        loss_data += loss
-        recall = recall_score(y_true, y_pred, average="macro", zero_division=0)
-        precision = precision_score(y_true, y_pred, average="macro", zero_division=0)
-        f1_data += f1_score(y_true, y_pred, average="macro")
-        f1_total += 1
-    acc = (100 * acc_data / f1_total)
-    f1_data = (100 * f1_data / f1_total)
-    acc_list.append(acc)
-    loss_list.append(loss_data)
-    f1_list.append(f1_data)
-    model.eval()
-    with torch.no_grad():
+def model_train():
+    pass
+
+
+if __name__ == "__main__":
+    # 1. 파일 읽기, x_data y_data 로 나눔
+    df = read_file(config.dataset_file_path)
+    x_data = df.iloc[:, :-2]  # y 값 제외하고 슬라이싱 , 고장단계도 제외(1,2,3 은 고장 유무가 0이고 4,5는 고장 유무가 1이기때문에 제거)
+    y_data = df.iloc[:, -1]  # y 값 만 슬라이싱
+    # 2. feature selection
+    x_data, selected_feat = my_feature_selection(x_data, y_data)
+
+    # 3. train test split
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data,
+                                                        test_size=0.2, stratify=y_data,
+                                                        random_state=32)
+    print(Counter(y_train))
+    print(Counter(y_test))
+    # 4. sampling
+    x_train, y_train = my_over_sampling(x_train, y_train)
+    # 5. scaler
+    x_train, x_test = my_scaler(x_train, x_test)
+    # ============================== make model =================================
+    # 6. covert to Tensor
+    x_train = torch.FloatTensor(x_train)
+    y_train = torch.FloatTensor(y_train)
+    x_test = torch.FloatTensor(x_test)
+    y_test = torch.FloatTensor(y_test.to_numpy())
+    # 7. make dataset
+    train_dataset = TensorDataset(x_train, y_train)
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    test_dataset = TensorDataset(x_test, y_test)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+
+    # hyper parameter
+    epoch = 300
+    lr = 0.1
+
+    #
+    loss_list = []  # loss 값을 저장할 리스트
+    acc_list = []  # acc 값을 저장할 리스트
+    f1_list = []  # f1 score 값을 저장할 리스트
+    #
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = Model()
+    model.to(device)  #
+    cost_func = nn.BCELoss()  # 이진분류기 때문에 binary cross entropy 사용
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.5)
+    for ep in range(1, epoch + 1):
+        acc_data = 0  # 1 epoch accuracy
+        loss_data = 0  # 1 epoch loss
+        f1_data = 0  # 1 epoch f1 score
+        recall_data = 0  # 1 epoch recall
+        precision_data = 0  # 1 epoch precision
+        total = 0
+        f1_total = 0
         for x, y in train_loader:
-            target = model(x)
-            y = y.reshape(-1,1)
-            loss = cost_func(target, y)
-            valid_loss = loss.item() * x.size(0)
-        valid_list.append(valid_loss)
+            x = x.to(device)
+            y = y.to(device)
+            pred = model(x)
+            loss = cost_func(pred, y.reshape(-1, 1))
+            optimizer.zero_grad()
+            loss.backward()  # backpropagation
+            optimizer.step()  # weight bias update
+            # 예측값이 0.5 이상은 1로 처리 0.5 이하는 0 로 처리
+            # 실제 정답값과 비교하여 accuracy 구함
+            y_pred = pred.cpu().detach().numpy()
+            y_pred = np.where(y_pred >= 0.5, 1, 0)
+            y_true = y.cpu().detach().numpy().reshape(-1, 1)
+            acc_data += np.sum(y_pred == y_true)
+            # accuracy 를 구하기 위해 전체 데이터 사이즈 더함
+            total += y.size(0)
+            # loss 값 더함
+            loss_data += loss
+            recall = recall_score(y_true, y_pred, average="weighted", zero_division=0)
+            precision = precision_score(y_true, y_pred, average="weighted", zero_division=0)
+            precision_data += precision
+            recall_data += recall
+            f1_data += f1_score(y_true, y_pred, average="weighted", zero_division=0)
+            f1_total += 1
+        acc = (100 * acc_data / total)
+        f1_data = (100 * f1_data / f1_total)
+        precision_data = (100 * precision_data / f1_total)
+        recall_data = (100 * recall_data / f1_total)
+        acc_list.append(acc)
+        loss_list.append(loss_data)
+        f1_list.append(f1_data)
+        if ep % 100 == 0:
+            print(
+                f"epoch : {ep}/{epoch}\t\tloss:{loss_data}\t\t acc:{acc:.3f} \t\t f1 score :{f1_data:.3f} \t\t recall data:{recall_data:.3f} \t\t precision data :{precision_data:.3f} ")
 
-    if ep % 100 == 0:
-        print(f"epoch : {ep}/{epoch}\t\tloss:{loss_data}\t\t acc:{acc} \t\t f1 score :{f1_data} ")
-        print(f'Epoch {ep} \t\t Training Loss: {loss_data} \t\t Validation Loss: {valid_loss}')
+    total = 0
+    acc = 0
+    f1_dict = {
+        "weighted": 0,
+        "macro": 0,
+        "micro": 0
+    }
+    test_loss = 0
+    f1_tot = 0
+    with torch.no_grad():
+        for x, y in test_loader:
+            x = x.to(device)
+            y = y.to(device)
+            pred = model(x)
 
-total = 0
-acc = 0
-f1_dict = {
-    "weighted": 0,
-    "macro": 0,
-    "micro": 0
-}
-test_loss = 0
-f1_tot = 0
-with torch.no_grad():
-    for x, y in test_loader:
-        pred = model(x)
+            y_pred = pred.cpu().detach().numpy()
+            y_pred = np.where(y_pred >= 0.5, 1, 0)
+            test_loss += cost_func(pred, y.reshape(-1, 1))
+            y_true = y.cpu().detach().numpy().reshape(-1, 1)
+            acc += np.sum(y_pred == y_true)
+            for key, value in f1_dict.items():
+                recall = recall_score(y_true, y_pred, average=key)
+                precision = precision_score(y_true, y_pred, average=key)
+                f1_dict[key] += 2 * ((recall * precision) / (recall + precision))
+            f1_tot += 1
+            total += y.size(0)
+    acc = (100 * acc / total)
+    print(f"accuracy: {acc:.2f}%")
+    for val, key in f1_dict.items():
+        print(f"f1 score {val} : {100 * key / f1_tot:.2f}%")
 
-        y_pred = pred.detach().numpy()
-        y_pred = np.where(y_pred >= 0.5, 1, 0)
-        test_loss += cost_func(pred, y.reshape(-1, 1))
-        y_true = y.detach().numpy().reshape(-1, 1)
-        acc += accuracy_score(y_true, y_pred)
-        for key, value in f1_dict.items():
-            recall = recall_score(y_true, y_pred, average=key)
-            precision = precision_score(y_true, y_pred, average=key)
-            f1_dict[key] += f1_score(y_true, y_pred, average=key)
-        f1_tot += 1
-        total += 1
-acc = (100 * acc / total)
-print(f"accuracy: {acc:.2f}%")
-for val, key in f1_dict.items():
-    print(f"f1 score {val} : {100 * key / f1_tot:.2f}%")
-
-
-"""valid train loss 값 비교"""
-fig, ax1 = plt.subplots()
-ax1.plot(loss_list,label="train_loss")
-ax1.plot(valid_list,label="valid_loss")
-fig.legend()
-plt.show()
-
-model_name = "model1.pt"
-torch.save(model.state_dict(),model_name)
-
-'''''
-fig, ax1 = plt.subplots()
-ax1.plot(acc_list, color='blue', label="accuracy")
-ax1.plot(f1_list, color="green", label="f1 score")
-ax1.set_ylabel("accuracy")
-ax2 = ax1.twinx()
-ax2.plot(loss_list, color='red', label="loss")
-ax2.set_ylabel("loss")
-ax1.set_xlabel("epoch")
-plt.title("oversampling SMOTE")
-fig.legend()
-plt.savefig("../img/oversampling_smote.png")
-'''''
+    torch.save(model.state_dict(),f"{model_name}.pth")
+# rus = RandomUnderSampler(random_state=42)
+# print(x_data, y_data)
+# x_data_rus, y_data_rus = rus.fit_resample(x_data, y_data)
+# print(x_data_rus, y_data_rus)
